3389de90f3bbf4aeebee2543d38d999ed4af557a
abot7613 <abottu25560@ucumberlands.edu>
2025-07-06 17:28:06 -0400

    second commit

--- MSCS532_Assignment1.py
+++ MSCS532_Assignment1.py
@@ -1,8 +1,11 @@
 def insertion_sort(array):
     n = len(array)
+    #loop through each element in the array starting from the second element
     for i in range(1, n):
         key = array[i]
         j = i - 1
+        # Move elements of array[0..i-1], that are greater than key,
+        # to one position ahead of their current position
         while j >= 0 and array[j] < key:
             array[j + 1] = array[j]
             j -= 1
